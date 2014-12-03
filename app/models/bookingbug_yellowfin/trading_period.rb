4b8cf9058100f1be7fb3e63fb48cdc07e49ed657
Joel Sturmfels <jsturmfels@bookingbug.com>
2014-12-03 11:54:55 +0000

    use ruby date not date in module

--- app/models/bookingbug_yellowfin/trading_period.rb
+++ app/models/bookingbug_yellowfin/trading_period.rb
@@ -5,7 +5,7 @@ module BookingbugYellowfin
       csv_text = File.read(csv_filepath)
       csv = CSV.parse(csv_text, :headers => false)
       csv.each do |row|
-        BookingbugYellowfin::TradingPeriod.create!(date: Date.parse(row[0]), yf_date: BookingbugYellowfin::FormatHelpers.to_yf_format(Date.parse(row[0])), year: row[1].to_i, month: row[2], period: row[3].to_i, week: row[4].to_i)
+        BookingbugYellowfin::TradingPeriod.create!(date: ::Date.parse(row[0]), yf_date: BookingbugYellowfin::FormatHelpers.to_yf_format(::Date.parse(row[0])), year: row[1].to_i, month: row[2], period: row[3].to_i, week: row[4].to_i)
       end
     end
   end
