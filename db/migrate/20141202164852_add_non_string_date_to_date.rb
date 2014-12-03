c7856e093b1138d9d5d9d06aa40150fa7e83da99
Joel Sturmfels <jsturmfels@bookingbug.com>
2014-12-03 11:03:35 +0000

    trading dates and periods model

--- db/migrate/20141202164852_add_non_string_date_to_date.rb
+++ db/migrate/20141202164852_add_non_string_date_to_date.rb
@@ -1,6 +1,6 @@
 class AddNonStringDateToDate < ActiveRecord::Migration
   def change
-    # add_column :bookingbug_yellowfin_dates, :typed_date, :date
+    add_column :bookingbug_yellowfin_dates, :typed_date, :date
 
     BookingbugYellowfin::Date.find_each() do |date|
       date.typed_date = Date.parse(date.date)
--- lib/bookingbug_yellowfin.rb
+++ lib/bookingbug_yellowfin.rb
@@ -1,6 +1,7 @@
 require "bookingbug_yellowfin/engine"
 require "bookingbug_yellowfin/format_helpers"
 require 'date'
+require 'csv'
 
 module BookingbugYellowfin
 end
