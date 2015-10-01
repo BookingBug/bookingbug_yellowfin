567e511c85ab31e8e5e15f06e1d4948ffee5b31f
Joel Sturmfels <jsturmfels@bookingbug.com>
2015-10-01 16:02:16 +0100

    fixes for changes to format of the report

--- app/models/bookingbug_yellowfin/booking_details.rb
+++ app/models/bookingbug_yellowfin/booking_details.rb
@@ -26,84 +26,85 @@ module BookingbugYellowfin
       e_days = (::Date.today - edate) if edate
       block = View::ReportsController::DATA_GROUPS[1][:reports][14]
       block[:date_field] = "updated_at"
+      fields = View::ReportsController::DATA_GROUPS[1][:reports][14][:fields]
       titles, rows = View::ReportsController.generate_report_data block,
                                                    company_id,
                                                    company_id.to_s,
                                                    {
                                                      :days=>days,
                                                      :e_days=>e_days, 
-                                                     :fields=>View::ReportsController::DATA_GROUPS[1][:reports][14][:fields]
+                                                     :fields=>fields
                                                    }
       for row in rows
-        next if Purchase::Item.find_by_id(row[19]).blank?
-        record = BookingbugYellowfin::BookingDetails.where( 
-          booking_id: row[0]
+        next if Purchase::Item.find_by_id(row[fields.find_index(:purchase_item_id)]).blank?
+        record = BookingbugYellowfin::BookingDetails.where(
+          booking_id: fields.find_index(:id)
           ).first
         if record.present?
-          record.update_attributes( 
-            booking_id: row[0],
-            slot_id: row[1],
+          record.update_attributes!( 
+            booking_id: row[fields.find_index(:id)],
+            slot_id: row[fields.find_index(:slot_id)],
             company_id: company_id,
-            booking_created_at: row[3],
-            yf_format_booking_created_at: BookingbugYellowfin::FormatHelpers.to_yf_format(row[3]), #yyyy/mm/dd
-            member_id: row[4],
-            service: row[5],
-            resource: row[6],
-            person: row[7],
-            session: row[8],
-            just_date: row[9],
-            yf_format_just_date: BookingbugYellowfin::FormatHelpers.to_yf_format(row[9]),
-            day_of_week: row[10],
-            month: row[11],
-            just_time: row[12],
-            duration: row[13],
-            price: row[14],
-            paid: row[15],
-            s_status: row[16],
-            coupon_code: row[17],
-            repeat_booking_id: row[18],
-            purchase_item_id: row[19],
-            cancellation_date: row[20],
-            late_cancel: row[21],
-            attended: row[22],
-            channel: row[24],
-            readable_current_multi_stat: row[25],
-            notes: row[26],
-            feedback: row[27],
-            pretty_print_multi_status: row[28]
+            booking_created_at: row[fields.find_index(:created_at)],
+            yf_format_booking_created_at: BookingbugYellowfin::FormatHelpers.to_yf_format(row[fields.find_index(:created_at)]), #yyyy/mm/dd
+            member_id: row[fields.find_index(:member_id)],
+            service: row[fields.find_index(:service_id)],
+            resource: row[fields.find_index(:resource_id)],
+            person: row[fields.find_index(:person_id)],
+            session: row[fields.find_index(:session)],
+            just_date: row[fields.find_index(:just_date)],
+            yf_format_just_date: BookingbugYellowfin::FormatHelpers.to_yf_format(row[fields.find_index(:just_date)]),
+            day_of_week: row[fields.find_index(:day_of_week)],
+            month: row[fields.find_index(:month)],
+            just_time: row[fields.find_index(:just_time)],
+            duration: row[fields.find_index(:duration)],
+            price: row[fields.find_index(:price)],
+            paid: row[fields.find_index(:paid)],
+            s_status: row[fields.find_index(:s_status)],
+            coupon_code: row[fields.find_index(:coupon_code)],
+            repeat_booking_id: row[fields.find_index(:repeat_booking_id)],
+            purchase_item_id: row[fields.find_index(:purchase_item_id)],
+            cancellation_date: row[fields.find_index(:cancellation_date)],
+            late_cancel: row[fields.find_index(:late_cancel)],
+            attended: row[fields.find_index(:attended)],
+            channel: row[fields.find_index(:channel)],
+            readable_current_multi_stat: row[fields.find_index(:readable_current_multi_stat)],
+            notes: row[fields.find_index(:notes)],
+            feedback: row[fields.find_index(:feedback)],
+            pretty_print_multi_status: row[fields.find_index(:pretty_print_multi_status)]
           )
         else
-          BookingbugYellowfin::BookingDetails.create( 
-            booking_id: row[0],
-            slot_id: row[1],
+          BookingbugYellowfin::BookingDetails.create!( 
+            booking_id: row[fields.find_index(:id)],
+            slot_id: row[fields.find_index(:slot_id)],
             company_id: company_id,
-            booking_created_at: row[3],
-            yf_format_booking_created_at: BookingbugYellowfin::FormatHelpers.to_yf_format(row[3]), #yyyy/mm/dd
-            member_id: row[4],
-            service: row[5],
-            resource: row[6],
-            person: row[7],
-            session: row[8],
-            just_date: row[9],
-            yf_format_just_date: BookingbugYellowfin::FormatHelpers.to_yf_format(row[9]),
-            day_of_week: row[10],
-            month: row[11],
-            just_time: row[12],
-            duration: row[13],
-            price: row[14],
-            paid: row[15],
-            s_status: row[16],
-            coupon_code: row[17],
-            repeat_booking_id: row[18],
-            purchase_item_id: row[19],
-            cancellation_date: row[20],
-            late_cancel: row[21],
-            attended: row[22],
-            channel: row[24],
-            readable_current_multi_stat: row[25],
-            notes: row[26],
-            feedback: row[27],
-            pretty_print_multi_status: row[28]
+            booking_created_at: row[fields.find_index(:created_at)],
+            yf_format_booking_created_at: BookingbugYellowfin::FormatHelpers.to_yf_format(row[fields.find_index(:created_at)]), #yyyy/mm/dd
+            member_id: row[fields.find_index(:member_id)],
+            service: row[fields.find_index(:service_id)],
+            resource: row[fields.find_index(:resource_id)],
+            person: row[fields.find_index(:person_id)],
+            session: row[fields.find_index(:session)],
+            just_date: row[fields.find_index(:just_date)],
+            yf_format_just_date: BookingbugYellowfin::FormatHelpers.to_yf_format(row[fields.find_index(:just_date)]),
+            day_of_week: row[fields.find_index(:day_of_week)],
+            month: row[fields.find_index(:month)],
+            just_time: row[fields.find_index(:just_time)],
+            duration: row[fields.find_index(:duration)],
+            price: row[fields.find_index(:price)],
+            paid: row[fields.find_index(:paid)],
+            s_status: row[fields.find_index(:s_status)],
+            coupon_code: row[fields.find_index(:coupon_code)],
+            repeat_booking_id: row[fields.find_index(:repeat_booking_id)],
+            purchase_item_id: row[fields.find_index(:purchase_item_id)],
+            cancellation_date: row[fields.find_index(:cancellation_date)],
+            late_cancel: row[fields.find_index(:late_cancel)],
+            attended: row[fields.find_index(:attended)],
+            channel: row[fields.find_index(:channel)],
+            readable_current_multi_stat: row[fields.find_index(:readable_current_multi_stat)],
+            notes: row[fields.find_index(:notes)],
+            feedback: row[fields.find_index(:feedback)],
+            pretty_print_multi_status: row[fields.find_index(:pretty_print_multi_status)]
           )
         end
       end
